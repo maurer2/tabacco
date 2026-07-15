77e7e3cbd99d543c360cf53190b6023683bb8f7d
Martin <mr.mmaurer@gmail.com>
2026-07-15 21:48:47 +0200

    Added Tablist markup

--- src/AccordionTabs.ts
+++ src/AccordionTabs.ts
@@ -26,7 +26,40 @@ export class AccordionTabs {
     return markup;
   }
 
+  // https://www.w3.org/WAI/ARIA/apg/patterns/tabs/
+  #getTabListMarkup() {
+    const markupTabs = `
+      <div role="tablist">
+        ${[...this.#data]
+          .map(([key]) => {
+            const isActive = key === this.#activeKey;
+
+            return `
+              <button role="tab" id="tab-${key}" aria-selected="${isActive ? 'true' : 'false'}" aria-controls="panel-${key}">${key}</button>
+            `;
+          })
+          .join('')}
+      </div>
+    `;
+
+    const markupPanel = [...this.#data]
+      .map(([key, data]) => {
+        const isActive = key === this.#activeKey;
+
+        return `
+          <article role="tabpanel" id="panel-${key}" aria-labelledby="tab-${key}" ${isActive ? '' : 'hidden'}>${data}</article>
+        `;
+      })
+      .join('');
+
+    return [markupTabs, markupPanel].join('');
+  }
+
   render(): void {
-    this.#domElement.innerHTML = this.#getAccordionMarkup();
+    this.#domElement.innerHTML = [
+      this.#getAccordionMarkup(),
+      '<hr/>',
+      this.#getTabListMarkup(),
+    ].join('');
   }
 }
