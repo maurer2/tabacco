3f94923e6668d3fb107ca213fc89d57b390970d2
Martin <mr.mmaurer@gmail.com>
2026-07-16 09:08:58 +0200

    Added handling of toggle events in accordion

--- src/AccordionTabs.ts
+++ src/AccordionTabs.ts
@@ -8,9 +8,26 @@ export class AccordionTabs {
     this.#data = data;
     this.#activeKey = startKey;
 
+    // toggle event does not bubble
+    this.#domElement.addEventListener('toggle', this.#handleToggle, { capture: true });
     // ignores click events from tab panels or accordion
     this.#domElement.addEventListener('click', this.#handleTabClick);
   }
+
+  // also called when a tab is expanded via ctrl+f search
+  #handleToggle = (event: ToggleEvent): void => {
+    const targetElement = event.target;
+
+    if (event.newState !== 'open' || !(targetElement instanceof HTMLDetailsElement)) {
+      return;
+    }
+
+    const newActiveKey = targetElement.dataset.key;
+    if (newActiveKey !== undefined && newActiveKey !== this.#activeKey) {
+      this.#activeKey = newActiveKey;
+      this.render();
+    }
+  };
 
   #handleTabClick = (event: MouseEvent): void => {
     const targetElement = event.target;
@@ -36,7 +53,7 @@ export class AccordionTabs {
       const isActive = key === this.#activeKey;
 
       return `
-        <details name="accordion" ${isActive ? 'open' : ''}>
+        <details name="accordion" data-key="${key}" ${isActive ? 'open' : ''}>
           <summary class="trigger">${key}</summary>
           <div class="content">${data}</div>
         </details>
